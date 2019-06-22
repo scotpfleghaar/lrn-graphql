4f5fd1e4cdcd4e98e5a790f6d0c1a34846be5c91
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-06-22 10:48:14 -0500

    QuickStart

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,3 +1,26 @@
-import { print } from './myModule';
+import { GraphQLServer } from 'graphql-yoga';
 
-print('Working!');
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The Server is Running')
+});
