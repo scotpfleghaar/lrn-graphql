de3a48df873664d974e6faf1a8c83a585365ad6b
scot.pfleghaar <scot.pfleghaar@gmail.com>
2019-06-22 14:46:46 -0500

    Scaler Types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,28 +3,33 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
-        name: String!
-        location: String!
-        bio: String!
+       id: ID!
+       name: String!
+       age: Int!
+       employed: Boolean!
+       gpa: Float
     }
 `;
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        id () {
+            return "abc123"
         },
-        name() {
-            return 'Scot'
+        name () {
+            return "Scot"
         },
-        location() {
-            return 'Houston'
+        age () {
+            return 24
         },
-        bio() {
-            return 'Canadian'
-        }
+        employed () {
+            return true
+        },
+        gpa () {
+            return null
+        },
+
     }
 };
 
